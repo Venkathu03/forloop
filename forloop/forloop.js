dac207df2589602066e159b7e50a93ccfa2ec47c
Vengadesh P <104545809+Venkathu03@users.noreply.github.com>
2022-07-09 13:13:07 +0530

    Add files via upload

---
+++ forloop/forloop.js
@@ -0,0 +1,32 @@
+// n number
+var a=15;
+console.log('value=',a);
+var m=0;
+
+for(let i=1; i<=20; i++){
+    m=i*a;
+    console.log('a=',m);
+}
+//
+
+
+
+// number series
+
+var range=5;
+for( var i=1; i<=range; i++){
+    for(var j=1; j<=i;j++){
+        console.log(i)
+        console.log('\t');
+    }
+}
+//
+
+
+
+//integer in word
+var x=34;
+ 
+for(x=34; x<=34; x++){
+    console.log(x,'three four');
+}
